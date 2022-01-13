f43d2b72f87a6b0a3923081a3cc62648e25ebf22
Nicolai <cph-nm172@cphbusiness.dk>
2022-01-13 19:22:27 +0100

    Fixed tests?

--- src/test/java/rest/LoginEndpointTest.java
+++ src/test/java/rest/LoginEndpointTest.java
@@ -26,7 +26,7 @@ import utils.EMF_Creator;
 public class LoginEndpointTest {
 
     private static final int SERVER_PORT = 7777;
-    private static final String SERVER_URL = "http://localhost/api";
+    private static final String SERVER_URL = "http://localhost/devops/api";
 
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
