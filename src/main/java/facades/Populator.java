fec51c8d252d5faa9536491eebdea90bc6ab1a31
Nicolai <cph-nm172@cphbusiness.dk>
2022-01-13 09:26:18 +0100

    fixed problems?

--- src/main/java/facades/Populator.java
+++ src/main/java/facades/Populator.java
@@ -26,18 +26,6 @@ public class Populator {
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
         EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        BoatFacade FACADE =  BoatFacade.getBoatFacade(emf);
-
-        System.out.println(FACADE.getHarbourByName("Thors havn"));
-        /*Owner owner = new Owner("Jens", "Vejvej 10", 12345678);
-        Owner owner1 = new Owner("Mikkel Hansen", "Gayvej 25", 11223344);
-        Harbour harbour = new Harbour("Havnhavn", "Havnhavnevejgade 25", 69);
-        Boat boat = new Boat("Ferrari", "Ferrari boat", "FerrariBoat", "https://www.boat24.com/fotos/xlarge/341796-abcbdb664172330ce1008fab221c58ad-x-1463319-5f43b6c9052ae4b5c128d4b2dedb7c31.jpg", harbour);
-        owner1.addBoat(boat);
-        em.persist(boat);
-        em.getTransaction().commit();
-        em.close();*/
     }
 
     public static void main(String[] args) {
--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -1,7 +1,5 @@
 package rest;
 
-import facades.OwnerFacade;
-
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
@@ -34,8 +32,6 @@ public class ApplicationConfig extends Application {
         resources.add(security.RolesAllowedFilter.class);
         resources.add(security.errorhandling.AuthenticationExceptionMapper.class);
         resources.add(security.errorhandling.NotAuthorizedExceptionMapper.class);
-        resources.add(OwnerResource.class);
-        resources.add(BoatResource.class);
     }
     
 }
